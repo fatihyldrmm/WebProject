c627da6c3042c37f208606bae4cbea9cf3e898fc
HalilTufan1 <htufanabc2003@gmail.com>
2023-12-22 12:27:38 +0300

    Login,register sayfalarının tasarımı

---
+++ Frontend/WebClient/WebClient/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebClient.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+    }
+}
