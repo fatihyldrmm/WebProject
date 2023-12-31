a87eef50cc9433428cbbda960bf45e94d0df920a
HalilTufan1 <htufanabc2003@gmail.com>
2023-12-31 23:43:50 +0300

    Controllerların yazılması

--- Backend/WebApi/Controllers/AppointmentController.cs
+++ Backend/WebApi/Controllers/AppointmentController.cs
@@ -13,8 +13,66 @@ namespace WebApi.Controllers
     [ApiController]
     public class AppointmentController : ControllerBase
     {
-        
-        
+        private readonly IServiceManager _serviceManager;
+        private readonly IRepositoryManager _repositoryManager;
+
+
+        public AppointmentController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        // GET: api/<AppointmentController>
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var appointments = _serviceManager.AppointmentService.GetAll();
+            return Ok(appointments);
+        }
+        [HttpGet]
+        [Route("GetByPatientId/{id}")]
+        public IActionResult GetByPatientId(Guid id)
+        {
+            var appointments = _serviceManager.AppointmentService.GetByIdAsync(id);
+            return Ok(appointments);
+        }
+        [HttpGet]
+        [Route("GetByDoctorId/{id}")]
+        public IActionResult GetByDoctorId(Guid id)
+        {
+            var appointments = _serviceManager.AppointmentService.GetByIdAsync(id);
+            return Ok(appointments);
+        }
+        [HttpPost]
+        [Route("addappointment")]
+        public IActionResult AddAppointment([FromBody] AppointmentCreateRequest model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (model == null)
+            {
+                return BadRequest();
+            }
+            var hasAlredyAppointment = _repositoryManager.AppointmentRepository.GetWhere(i => i.DoctorId == model.DoctorId && i.AppointmentDay == model.AppointmentDay);
+            if (hasAlredyAppointment != null)
+            {
+                return StatusCode(409, "Bu tarihte zaten bir randevu bulunmaktadır.");
+            }
+            var appointment = new Appointment
+            {
+                Id = Guid.NewGuid(),
+                PatientId = model.PatientId,
+                DoctorId = model.DoctorId,
+                AppointmentDay = model.AppointmentDay
+            };
+            appointment.Doctor = _repositoryManager.DoctorRepository.GetByIdAsync(appointment.DoctorId.ToString()).Result;
+            appointment.Patient = _repositoryManager.PatientRepository.GetByIdAsync(appointment.PatientId.ToString()).Result;
+            _repositoryManager.AppointmentRepository.AddAsync(appointment);
+            return Ok();
+        }
+
 
     }
 }
--- Frontend/WebClient/WebClient/Areas/User/Controllers/AppointmentController.cs
+++ Frontend/WebClient/WebClient/Areas/User/Controllers/AppointmentController.cs
@@ -1,10 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebClient.Models.ViewModels.Appointment;
 
 namespace WebClient.Areas.User.Controllers
 {
     public class AppointmentController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult RandevuAl()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RandevuAl(AppointmentCreateVM model)
         {
             return View();
         }
